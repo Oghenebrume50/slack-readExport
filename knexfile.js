8e4151f40c56c5740a34b55ca4497d7e712d92a2
oghenebrume50 <raphlbrume@gmail.com>
2020-04-06 21:01:39 +0100

    changed connection from object to string

--- knexfile.js
+++ knexfile.js
@@ -16,9 +16,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
-      database: process.env.DATABASE_URL
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
