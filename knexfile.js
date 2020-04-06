bde37c76152fa624d7f20254e331105624fe71e6
oghenebrume50 <raphlbrume@gmail.com>
2020-04-06 18:43:11 +0100

    change to string pg connection for production

--- knexfile.js
+++ knexfile.js
@@ -17,16 +17,14 @@ module.exports = {
   production: {
     client: 'pg',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: process.env.DATABASE_URL
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './dbschema',
+    },
   }
 };
