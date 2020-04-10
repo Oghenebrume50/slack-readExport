d280d285fd84ba895b4bf19222d14534a87438d2
oghenebrume50 <raphlbrume@gmail.com>
2020-04-10 19:44:23 +0100

    added the heroku url

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const botMention = require('./utils/bot_mention');
 const check = require('./utils/check');
 const logger = require('./utils/logger');
 
-const urlhost = "https://fdf0e3e0.ngrok.io/";
+const urlhost = "https://slack-readexport.herokuapp.com/";
 
 bot.event("app_mention", async ({ context, event }) => {
   try {
